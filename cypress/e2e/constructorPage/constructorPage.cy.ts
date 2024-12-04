166c83c139af36f084af709eadfef22baa46dde9
Talissa <liayai@ya.ru>
2024-12-04 18:11:44 +0300

    feat: add test to choose ingridients

--- cypress/e2e/constructorPage/constructorPage.cy.ts
+++ cypress/e2e/constructorPage/constructorPage.cy.ts
@@ -1,26 +1,34 @@
 /// <reference types="cypress" />
-import { ingredients } from 'cypress/fixtures/ingredients';
-describe('проверяем доступность приложения', function () {
-  beforeEach(() => {});
-
-  it('сервис должен быть доступен по адресу localhost:4000', () => {
+describe('constructor page', function () {
+  it('should intercept getIngredients request', () => {
     cy.intercept('GET', 'https://norma.nomoreparties.space/api/ingredients', {
-      fixture: ingredients
+      fixture: '../fixtures/ingredients.json'
     });
-    //cy.wait('@getIngredients');
     cy.visit('http://localhost:4000');
-
-    //cy.log(@getIngredients.response.body);
   });
 
-  // it('после клика должен срабатывать alert', () => {
-  //   const button = cy.get(`[data-cy='order-button']`);
-  //   button.contains('Оформить заказ');
-  //   button.click();
-  //   window.alert('Выберите ингредиенты');
-  // });
+  describe('constructor burger', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:4000');
+      cy.viewport(1920, 1080);
+    });
 
-  // it('клик на ингредиент должен добавлять его в конструктор', () => {
-  //   cy.get('Добавить').click();
-  // });
+    it('should add the bun, staffing and sause by click', () => {
+      cy.contains('li', 'Краторная булка').find('button').click();
+      //burger.get('span:contains("Краторная булка")').should('not.exist');
+      // burger
+      //   .get('span:contains("Флюоресцентная булка")')
+      //   .its('length')
+      //   .should('eq', 2);
+      cy.contains('div', 'Начинки').click();
+      cy.contains('li', 'Говяжий метеорит').find('button').click();
+      cy.contains('li', 'Биокотлета из марсианской Магнолии')
+        .find('button')
+        .click();
+      cy.contains('div', 'Булки').click();
+      cy.contains('li', 'Флюоресцентная булка').find('button').click();
+      // cy.contains('div', 'Соусы').click();
+      // cy.contains('li', 'Соус фирменный').find('button').click();
+    });
+  });
 });
